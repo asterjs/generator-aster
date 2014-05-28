5567658c0a5c0673639caac54817b4e674e86952
Ingvar Stepanyan <me@rreverser.com>
2014-05-28 17:03:10 +0300

    Updated sample and test.

--- app/templates/test/test.js
+++ app/templates/test/test.js
@@ -6,10 +6,30 @@ var assert = require('assert'),
 	Rx = require('rx'),
 	<%= varName %> = require('..');
 
-describe('Initialization', function () {
-	// Put initialization-safety tests here if needed
+it('test', function (done) {
+	var input = [{
+			type: 'File',
+			program: {
+				type: 'Program',
+				body: [/* put statements here */]
+			},
+			loc: {
+				source: 'file.js'
+			}
+		}],
+		expected = [{
+			type: 'File',
+			program: {
+				type: 'Program',
+				body: [/* put output statements here */]
+			},
+			loc: {
+				source: 'file.js'
+			}
+		}];
+
+	<%= varName %>(Rx.Observable.fromArray(input))
+	.zip(expected, assert.deepEqual)
+	.subscribe(function () {}, done, done);
 });
 
-describe('Transformation', function () {
-	// Put transformation tests here
-});
